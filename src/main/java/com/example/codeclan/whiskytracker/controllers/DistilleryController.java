76d45c719ff89b6175ae25d885352fcc332fc414
Cameron Maclean <cm.maclean@yahoo.com>
2021-02-10 21:31:33 +0000

    Added method to find distillery by region

--- src/main/java/com/example/codeclan/whiskytracker/controllers/DistilleryController.java
+++ src/main/java/com/example/codeclan/whiskytracker/controllers/DistilleryController.java
@@ -5,6 +5,7 @@ import com.example.codeclan.whiskytracker.repositories.DistilleryRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -17,8 +18,14 @@ public class DistilleryController {
     DistilleryRepository distilleryRepository;
 
     @GetMapping(value = "/distilleries")
-    public ResponseEntity<List<Distillery>> getAllDistilleries(){
-        List<Distillery> allDistilleries = distilleryRepository.findAll();
-        return new ResponseEntity<>(allDistilleries, HttpStatus.OK);
+    public ResponseEntity<List<Distillery>> getAllDistilleries(
+            @RequestParam(name = "region", required = false) String region
+    ){
+        if (region != null){
+            return new ResponseEntity<>(distilleryRepository.findByRegionIgnoreCase(region), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(distilleryRepository.findAll(), HttpStatus.OK);
+
     }
 }
